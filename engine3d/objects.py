f83fd1f7fdbd4ece386c8ad4acf79040aee409af
Matteo Landi <landimatte@gmail.com>
2010-04-09 12:35:51 +0200

    Draw triangles by default.
    The cube's faces should now be defined as triangular polygons.

--- engine3d/objects.py
+++ engine3d/objects.py
@@ -2,15 +2,6 @@
 
 from OpenGL import GL as gl
 
-def draw_quads(obj):
-  gl.glBegin(gl.GL_QUADS)
-  for (face, normal, color) in zip(obj.faces, obj.normals, obj.colors):
-    gl.glNormal3fv(normal)
-    gl.glColor3fv(color)
-    for p in face:
-      gl.glVertex3fv(obj.points[p])
-  gl.glEnd()
-
 class Object(object):
   def render(self):
     (ax, ay, az) = self.angle
@@ -28,6 +19,15 @@ class Object(object):
   def rotate(self, angle):
     self.angle = map(sum, zip(self.angle, angle))
 
+  def draw(self):
+    gl.glBegin(gl.GL_TRIANGLES)
+    for (face, normal, color) in zip(self.faces, self.normals, self.colors):
+      gl.glNormal3fv(normal)
+      gl.glColor3fv(color)
+      for p in face:
+        gl.glVertex3fv(self.points[p])
+    gl.glEnd()
+
 class Cube(Object):
   def __init__(self, center, angle, r=1):
     self.center = center
@@ -37,30 +37,27 @@ class Cube(Object):
                    (r, -r, -r), (r, r, -r),
                    (-r, r, -r), (-r, -r, -r),
                   )
-    self.faces = ((0, 1, 2, 3),
-                  (3, 2, 6, 7),
-                  (7, 6, 5, 4),
-                  (4, 5, 1, 0),
-                  (1, 5, 6, 2),
-                  (4, 0, 3, 7),
+    self.faces = ((0, 1, 2), (2, 3, 0),
+                  (3, 2, 6), (6, 7, 3),
+                  (7, 6, 5), (5, 4, 7),
+                  (4, 5, 1), (1, 0, 4),
+                  (1, 5, 6), (6, 2, 1),
+                  (4, 0, 3), (3, 7, 4),
                  )
-    self.normals = ((0, 0, 1),
-                    (-1, 0, 0),
-                    (0, 0, -1),
-                    (1, 0, 0),
-                    (0, 1, 0),
-                    (0, -1, 0),
+    self.normals = ((0, 0, 1), (0, 0, 1),
+                    (-1, 0, 0), (-1, 0, 0),
+                    (0, 0, -1), (0, 0, -1),
+                    (1, 0, 0), (1, 0, 0),
+                    (0, 1, 0), (0, 1, 0),
+                    (0, -1, 0), (0, -1, 0),
                    )
-    self.colors = ((0, 0, 1),
-                   (0, 1, 0),
-                   (0, 1, 1),
-                   (1, 0, 0),
-                   (1, 0, 1),
-                   (1, 1, 0),
+    self.colors = ((0, 0, 1), (0, 0, 1),
+                   (0, 1, 0), (0, 1, 0),
+                   (0, 1, 1), (0, 1, 1),
+                   (1, 0, 0), (1, 0, 0),
+                   (1, 0, 1), (1, 0, 1),
+                   (1, 1, 0), (1, 1, 0),
                   )
-
-  def draw(self):
-    draw_quads(self)
     
 class Wall(Object):
   def __init__(self, center, angle, width, height):
@@ -71,12 +68,9 @@ class Wall(Object):
                    (-width / 2, height / 2, 0),
                    (-width / 2, -height / 2, 0),
                   )
-    self.faces = ((0, 1, 2, 3),
+    self.faces = ((0, 1, 2), (2, 3, 0),
                  )
-    self.normals = ((0, 0, 1),
+    self.normals = ((0, 0, 1), (0, 0, 1),
                    )
-    self.colors = ((.6, .6, .6),
+    self.colors = ((.6, .6, .6), (.6, .6, .6),
                   )
-
-  def draw(self):
-    draw_quads(self)
